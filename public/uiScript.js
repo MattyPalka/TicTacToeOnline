f463ccdcf4114b669044196c508649387382a182
MattyPalka <mateuszpalka90@gmail.com>
2020-03-16 21:40:28 +0000

    added basic html setup

---
+++ public/uiScript.js
@@ -0,0 +1,7 @@
+let wHeight = $(window).height()
+let wWidth = $(window).width()
+
+let canvas = document.querySelector('#the-canvas')
+let context = canvas.getContext('2d')
+
+canvas.height = canvas.width
